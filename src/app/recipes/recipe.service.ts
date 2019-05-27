b64796c5899d4b6522db91e1088406c79c02a93e
John <zcj1993lif@gmail.com>
2019-05-27 17:33:45 -0400

    add ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,19 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is a test', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'),
-        new Recipe('Another test recipe', 'This is another test', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg')
+        new Recipe('A test recipe', 'This is a test', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg', [
+            new Ingredient('Meat', 1),
+            new Ingredient('Vegetable', 4)
+        ]),
+        new Recipe('Another test recipe', 'This is another test', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg',[
+            new Ingredient('Meat', 1),
+            new Ingredient('Vegetable', 5),
+            new Ingredient('Buns', 2)
+        ])
     ];
 
     getRecipes() {
